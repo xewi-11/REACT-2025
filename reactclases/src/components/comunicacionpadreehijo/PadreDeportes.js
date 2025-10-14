0f50d0c0f3f96eabedeb99e80d2fe112e3418229
xewi-11 <javi11coimbra@gmail.com>
2025-10-14 14:22:53 +0200

    cambios padre

--- reactclases/src/components/comunicacionpadreehijo/PadreDeportes.js
+++ reactclases/src/components/comunicacionpadreehijo/PadreDeportes.js
@@ -4,12 +4,20 @@ import HijoDeportes from "./HijoDeportes";
 class PadreDeportes extends Component{
    
     deportes=['futbol','baloncesto','volleyball','padel'];
-   
+   state={
+        favorito:""
+    }
+     mostrarFavorito=(nombre)=>{
+        this.setState({
+            favorito:nombre
+        })
+    }
     render(){
         return(<div>
             <h1>Soy el padre</h1>
+             <h2 style={{background:"gold"}}>Su deporte favorito es: {this.state.favorito}</h2>
             { this.deportes.map((deporte,index)=>{
-               return (<HijoDeportes key={index} nombre={deporte}/>)
+               return (<HijoDeportes key={index} nombre={deporte} callParent={this.mostrarFavorito}/>)
             })}
            
 
