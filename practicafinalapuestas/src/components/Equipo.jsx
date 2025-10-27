031378b3b6f4db644719de5d3c78402ba3d80056
xewi-11 <javi11coimbra@gmail.com>
2025-10-27 09:33:27 +0100

    feat: Refactor componentes Equipo e InsertarApuesta; mejorar la estructura del código y agregar manejo de errores en la inserción de apuestas

--- practicafinalapuestas/src/components/Equipo.jsx
+++ practicafinalapuestas/src/components/Equipo.jsx
@@ -1,6 +1,6 @@
-import axios from 'axios';
-import React, { Component } from 'react'
-import { useNavigate } from 'react-router-dom';
+import axios from "axios";
+import React, { Component } from "react";
+import { useNavigate } from "react-router-dom";
 
 const BotonJugadores = ({ idEquipo }) => {
   const navigate = useNavigate();
@@ -13,46 +13,49 @@ const BotonJugadores = ({ idEquipo }) => {
 
 const BotonVolver = () => {
   const navigate = useNavigate();
-  return (
-    <button onClick={() => navigate("/")}>
-      Volver
-    </button>
-  );
+  return <button onClick={() => navigate("/")}>Volver</button>;
 };
 
 export default class Equipo extends Component {
-  url="https://apiapuestas.azurewebsites.net/api/Equipos/";
-  state={
-    equipo:{}
-  }
-  cargarInformacionEquipo=()=>{
-     axios.get(this.url+this.props.idEquipo).then((result) => {
-       console.log(result.data);
-       this.setState({ equipo: result.data });
-     }).catch((err) => {
+  url = "https://apiapuestas.azurewebsites.net/api/Equipos/";
+  state = {
+    equipo: {},
+  };
+  cargarInformacionEquipo = () => {
+    axios
+      .get(this.url + this.props.idEquipo)
+      .then((result) => {
+        console.log(result.data);
+        this.setState({ equipo: result.data });
+      })
+      .catch((err) => {
         console.log(err);
-     });
-  }
-  componentDidMount=()=>{
+      });
+  };
+  componentDidMount = () => {
     this.cargarInformacionEquipo();
-  }
-  componentDidUpdate=(oldProps)=>{
-    if(oldProps.idEquipo!=this.props.idEquipo){
+  };
+  componentDidUpdate = (oldProps) => {
+    if (oldProps.idEquipo != this.props.idEquipo) {
       this.cargarInformacionEquipo();
     }
-  }
+  };
 
   render() {
     return (
       <div>
         <h1>{this.state.equipo.nombre}</h1>
-        <img src={this.state.equipo.imagen} alt={this.state.equipo.nombre} />
+        <img
+          src={this.state.equipo.imagen}
+          alt={this.state.equipo.nombre}
+          style={{ width: "150px", height: "150px" }}
+        />
         <h3>Champions: {this.state.equipo.champions}</h3>
         <p>{this.state.equipo.descripcion}</p>
 
         <BotonJugadores idEquipo={this.props.idEquipo} />
         <BotonVolver />
       </div>
-    )
+    );
   }
 }
