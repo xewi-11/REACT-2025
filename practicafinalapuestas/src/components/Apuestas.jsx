c0f5c13f48937472de6d1e6ef849733faa3582c9
xewi-11 <javi11coimbra@gmail.com>
2025-10-28 19:44:22 +0100

    refactor: Eliminar importación no utilizada de Navigate en Apuestas.jsx

--- practicafinalapuestas/src/components/Apuestas.jsx
+++ practicafinalapuestas/src/components/Apuestas.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import React, { Component } from 'react'
-import { Navigate, useNavigate } from 'react-router-dom';
+import {  useNavigate } from 'react-router-dom';
 
  function RealizarApuesta(){
         const navigate=useNavigate();
